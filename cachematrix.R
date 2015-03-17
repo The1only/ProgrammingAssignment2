ae67564981f478400f44731f29fbc357bd59d784
Terje Nilsen <teni@9tek.no>
2015-03-18 00:17:24 +0100

    Added a little more text

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,12 @@
-## To save compute time we store the inverse of an matrix
+## Matrix Inverse and store library v1.0...
+## To save compute time we store the inverse of the matrix...
+## Every time the content of the matrix changes, the user must call .set(y) with the new matrix
+## and the next time the inverse is requested it will again calculate.
 
-## makeCacheMatrix generats a matrix object and inverse it...
+## makeCacheMatrix() generates a matrix object and inverse it...
 ## The inverse is stored for later use, so only the first time it
 ## is requested is it calculated...
+
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
     set <- function(y) {
@@ -18,8 +22,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## cacheSolve gets the inverse of a matrix object, if it is already 
+## cacheSolve() gets the inverse of a matrix object, if it is already 
 ## calculated it will just get the last ansver to save time... 
+
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
     m <- x$getinv()
